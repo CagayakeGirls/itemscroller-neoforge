c857d6be39f49daecaa3513fcb08263ce3e4b20c
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-10-26 13:26:43 +0800

    clean code

--- src/main/java/team/cagayakegirls/rocknroller/RocknRoller.java
+++ src/main/java/team/cagayakegirls/rocknroller/RocknRoller.java
@@ -2,19 +2,17 @@ package team.cagayakegirls.rocknroller;
 
 import fi.dy.masa.itemscroller.ItemScroller;
 import fi.dy.masa.itemscroller.compat.modmenu.ModMenuImpl;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(RocknRoller.MOD_ID)
+@Mod(value = RocknRoller.MOD_ID, dist = Dist.CLIENT)
 public class RocknRoller {
     public static final String MOD_ID = "rocknroller";
 
     public RocknRoller(ModContainer modContainer) {
-        if (FMLLoader.getDist().isClient()) {
-            modContainer.registerExtensionPoint(IConfigScreenFactory.class, new ModMenuImpl().getModConfigScreenFactory());
-            new ItemScroller().onInitialize();
-        }
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, new ModMenuImpl().getModConfigScreenFactory());
+        new ItemScroller().onInitialize();
     }
 }
